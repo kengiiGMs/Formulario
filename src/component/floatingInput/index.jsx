08448f838ccbe376a0353d2e05b38790d153e3dc
kengiiGMs <kaikiaoto22@gmail.com>
2025-05-07 15:30:00 -0300

    refactor(component): Add onChange handler to floatingInput component

--- src/component/floatingInput/index.jsx
+++ src/component/floatingInput/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const FloatingInput = ({ type, id, placeholder }) => {
+const FloatingInput = ({ type, id, placeholder, onChange }) => {
     return (
         <div class="form-floating mb-3">
-            <input type={type} class="form-control" id={id} placeholder={placeholder} />
+            <input type={type} class="form-control" id={id} placeholder={placeholder} onChange={onChange} />
             <label for={id}>{placeholder}</label>
         </div>
     )
